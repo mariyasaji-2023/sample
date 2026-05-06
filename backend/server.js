e801a0aceec3e298c5772373152e06e112615e74
mariyasaji-2023 <mariyasajichiramel.com>
2026-05-06 21:58:33 +0530

    cors error

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
-const cors = require('cors');
+const cors = require("cors");
 
 const app = express();
 
-app.use(express.json());
-  app.use(cors({
-  origin: 'http://localhost:5173', // your frontend URL
-  credentials: true
-}));
+app.use(cors());
 
 
 // ✅ Connect MongoDB
