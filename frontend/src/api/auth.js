791f7e4061a874ed28ae88410af1863fb78dcf46
mariyasaji-2023 <mariyasajichiramel.com>
2026-05-06 22:06:43 +0530

    path

--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // const API = axios.create({ baseURL: '/api/auth' });
 const API = axios.create({
-  baseURL: 'https://sample-7pa7.onrender.com'
+  baseURL: 'https://sample-7pa7.onrender.com/api/auth'
 });
 
 export const register = (data) => API.post('/register', data);
